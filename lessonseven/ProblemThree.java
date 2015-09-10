193da19bc2d6a93d8260e55c7aa5653c3b763661
ambesgit <ambestetemke@gmail.com>
2015-09-10 18:34:37 -0500

    Problem three updated

--- lessonseven/ProblemThree.java
+++ lessonseven/ProblemThree.java
@@ -2,6 +2,7 @@ package lessonseven;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,42 +18,39 @@ public class ProblemThree extends JFrame{
 	JTextField tm,tp,tg,tf,tkm,tkg,tl,tc;
 	ProblemThree(){
 		setTitle("Metric Conversion Assitant");
-		setSize(600,300);
+		setSize(700,300);
 		setResizable(false);
 		setLocation(100,100);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new FlowLayout(FlowLayout.RIGHT));
-		JPanel p1=new JPanel(new BorderLayout());		
-		JPanel p2=new JPanel(new BorderLayout());
-		JPanel p3=new JPanel(new BorderLayout());		
-		JPanel p4=new JPanel(new BorderLayout());
-		JPanel p5=new JPanel(new BorderLayout());		
-		JPanel p6=new JPanel(new BorderLayout());
-		JPanel p7=new JPanel(new BorderLayout());		
-		JPanel p8=new JPanel(new BorderLayout());
-		JPanel p9=new JPanel(new BorderLayout());		
-		add(p1);add(p2);add(p3);add(p3);add(p4);add(p5);add(p6);add(p7);add(p8);add(p9);
+		setLayout(new FlowLayout());
+		JPanel p1=new JPanel(new GridLayout(8,2));		
+		JPanel p2=new JPanel(new GridLayout(8,2));
+		JPanel p3=new JPanel(new BorderLayout());	
+		add(p1);add(p2);add(p3);
 		lm=new JLabel("Mile:");lp=new JLabel("Pound:");	lg=new JLabel("Gallon:");	lf=new JLabel("Fahrenheit:");
 		lkm=new JLabel("Kilometer:");lkg=new JLabel("Kilogram:");ll=new JLabel("Liter:");lc=new JLabel("Centigrade:");
 		
 		tm=new JTextField(" ",20);tp=new JTextField(" ",20);tg=new JTextField(" ",20);tf=new JTextField(" ",20);
 		tkm=new JTextField(" ",20);tkg=new JTextField(" ",20);tl=new JTextField(" ",20);tc=new JTextField(" ",20);
-		convert=new JButton("Convert");
-		
-		//mile to killometer
-		p1.add(lm,"West");p1.add(tm,"East");
-		p2.add(lkm,"West");p2.add(tkm,"East");
-		//pound to killogram
-		p3.add(lp,"West");p3.add(tp,"East");
-		p4.add(lkg,"West");p4.add(tkg,"East");
-		//
-		p5.add(lg,"West");p5.add(tg,"East");
-		p8.add(lc,"West");p8.add(tc,"East");
-		//
-		p6.add(ll,"West");p6.add(tl,"East");
-		p7.add(lf,"West");p7.add(tf,"East");		
-		//submit button 
-		p9.add(convert,"Center");		
+		convert=new JButton("Convert");	
+		p1.add(lm);
+		p1.add(tm);
+		p1.add(lp);
+		p1.add(tp);
+		p1.add(lg);
+		p1.add(tg);
+		p1.add(lf);
+		p1.add(tf);
+		p2.add(lkm);		
+		p2.add(tkm);		
+		p2.add(lkg);
+		p2.add(tkg);	
+		p2.add(ll);
+		p2.add(tl);
+		p2.add(lc);
+		p2.add(tc);		
+		p3.add(convert);		
 		setVisible(true);
 		convert.addActionListener(new ActionListener(){
 			@Override
